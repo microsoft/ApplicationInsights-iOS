21dc49fe42e33cb39848efc50588775bff1ab8c0
Lukas Spieß <lspiess@microsoft.com>
2015-01-16 17:48:42 +0100

    Update TelemetryContext initializer and dictionary

--- Classes/MSAITelemetryContext.m
+++ Classes/MSAITelemetryContext.m
@@ -3,17 +3,39 @@
 
 @implementation MSAITelemetryContext
 
-- (instancetype)initWithInstrumentationKey:(NSString *)instrumentationKey endpointPath:(NSString *)endpointPath{
+- (instancetype)initWithInstrumentationKey:(NSString *)instrumentationKey
+                              endpointPath:(NSString *)endpointPath
+                        applicationContext:(MSAIApplication *)applicationContext
+                             deviceContext:(MSAIDevice *)deviceContext
+                           locationContext:(MSAILocation *)locationContext
+                            sessionContext:(MSAISession *)sessionContext
+                               userContext:(MSAIUser *)userContext
+                           internalContext:(MSAIInternal *)internalContext
+                          operationContext:(MSAIOperation *)operationContext{
   if ((self = [self init])) {
-    self.instrumentationKey = instrumentationKey;
-    self.endpointPath = endpointPath;
+    _instrumentationKey = instrumentationKey;
+    _endpointPath = endpointPath;
+    _application = applicationContext;
+    _device = deviceContext;
+    _location = locationContext;
+    _session = sessionContext;
+    _user = userContext;
+    _internal = internalContext;
+    _operation = operationContext;
   }
   return self;
 }
 
 - (NSDictionary *)contextDictionary{
+  NSMutableDictionary *contextDictionary = [self.application serializeToDictionary];
+  [contextDictionary addEntriesFromDictionary:[self.device serializeToDictionary]];
+  [contextDictionary addEntriesFromDictionary:[self.location serializeToDictionary]];
+  [contextDictionary addEntriesFromDictionary:[self.session serializeToDictionary]];
+  [contextDictionary addEntriesFromDictionary:[self.user serializeToDictionary]];
+  [contextDictionary addEntriesFromDictionary:[self.internal serializeToDictionary]];
+  [contextDictionary addEntriesFromDictionary:[self.operation serializeToDictionary]];
   
-  return [NSDictionary new];
+  return contextDictionary;
 }
 
 @end
