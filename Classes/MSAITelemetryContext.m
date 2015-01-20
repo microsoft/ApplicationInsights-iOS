4017e733eddf186252709d4181c765d284a72726
Christoph Wendt <chwend@microsoft.com>
2015-01-20 15:36:00 +0100

    Add session renewal

--- Classes/MSAITelemetryContext.m
+++ Classes/MSAITelemetryContext.m
@@ -1,7 +1,17 @@
 #import "MSAITelemetryContext.h"
 #import "MSAIHelper.h"
 
-@implementation MSAITelemetryContext
+
+static NSInteger defaultSessionRenewalMs = 30 * 60 * 1000; // 30 minutes
+static NSInteger defaultSessionExpirationMs = 24 * 60 * 60 * 1000; // 24 hours
+
+NSString *const kMSAITelemetrySessionId = @"MSAITelemetrySessionId";
+NSString *const kMSAISessionAcquisitionTime = @"MSAISessionAcquisitionTime";
+
+@implementation MSAITelemetryContext{
+  long _acquisitionMs;
+  long _renewalMs;
+}
 
 - (instancetype)initWithInstrumentationKey:(NSString *)instrumentationKey
                               endpointPath:(NSString *)endpointPath
@@ -27,15 +37,42 @@
 }
 
 - (NSDictionary *)contextDictionary{
+  
   NSMutableDictionary *contextDictionary = [self.application serializeToDictionary];
+  
+  [self updateSessionContext];
+  [contextDictionary addEntriesFromDictionary:[self.session serializeToDictionary]];
   [contextDictionary addEntriesFromDictionary:[self.device serializeToDictionary]];
   [contextDictionary addEntriesFromDictionary:[self.location serializeToDictionary]];
-  [contextDictionary addEntriesFromDictionary:[self.session serializeToDictionary]];
   [contextDictionary addEntriesFromDictionary:[self.user serializeToDictionary]];
   [contextDictionary addEntriesFromDictionary:[self.internal serializeToDictionary]];
   [contextDictionary addEntriesFromDictionary:[self.operation serializeToDictionary]];
   
   return contextDictionary;
+}
+
+- (void)updateSessionContext {
+  long currentDateMs = [[NSDate date] timeIntervalSince1970];
+  
+  BOOL firstSession = _acquisitionMs == 0 || _renewalMs == 0;
+  BOOL acqExpired = (currentDateMs  - _acquisitionMs) > defaultSessionExpirationMs;
+  BOOL renewalExpired = (currentDateMs - _renewalMs) > defaultSessionRenewalMs;
+  
+  [_session setIsFirst: (firstSession ? @"true" : @"false")];
+  
+  if (firstSession || acqExpired || renewalExpired) {
+    [_session setSessionId:[_device deviceId]];
+    [_session setIsFirst:@"true"];
+    
+    _renewalMs = currentDateMs;
+    _acquisitionMs = currentDateMs;
+    
+    [[NSUserDefaults standardUserDefaults] setValue:[_session sessionId] forKey:kMSAITelemetrySessionId];
+    [[NSUserDefaults standardUserDefaults] setInteger:_acquisitionMs forKey:kMSAISessionAcquisitionTime];
+  }else{
+    _renewalMs = currentDateMs;
+    [_session setIsFirst:@"false"];
+  }
 }
 
 @end
