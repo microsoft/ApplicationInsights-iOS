fdebb93e0395d71611f68f783463d8bdb6cc01f4
Christoph Wendt <chwend@microsoft.com>
2015-01-21 15:36:54 +0100

    Add sender class

---
+++ Classes/MSAISender.m
@@ -0,0 +1,13 @@
+//
+//  MSAISender.m
+//  AppInsights-iOS
+//
+//  Created by GrafZahl on 21/01/15.
+//
+//
+
+#import "MSAISender.h"
+
+@implementation MSAISender
+
+@end
---
+++ Classes/MSAISenderPrivate.h
@@ -0,0 +1,13 @@
+//
+//  MSAISenderPrivate.h
+//  AppInsights-iOS
+//
+//  Created by GrafZahl on 21/01/15.
+//
+//
+
+#ifndef AppInsights_iOS_MSAISenderPrivate_h
+#define AppInsights_iOS_MSAISenderPrivate_h
+
+
+#endif
