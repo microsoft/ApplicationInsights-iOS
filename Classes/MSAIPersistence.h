883344046070dc3177a98b5f4b5699a7a50a53e3
Benjamin Reimold <bereimol@microsoft.com>
2015-02-09 13:39:43 +0100

    Add some documentation for persistence class

--- Classes/MSAIPersistence.h
+++ Classes/MSAIPersistence.h
@@ -6,7 +6,30 @@
 
 @interface MSAIPersistence : NSObject
 
+
+///-----------------------------------------------------------------------------
+/// @name Save bundle of data
+///-----------------------------------------------------------------------------
+
+/**
+*  Prepares manager for sending out data.
+*
+*  @param bundle a bundle of tracked events (telemetry, crashes, ...) that will be serialized and saved
+*/
 + (void)persistBundle:(NSArray *)bundle;
+
+
+///-----------------------------------------------------------------------------
+/// @name Get a bundle of saved data
+///-----------------------------------------------------------------------------
+
+
+/**
+* Returns an arbitrary bundle of previously saved data from disk and deletes it.
+* Returns 'nil' if no bundle is available
+*/
+
+
 + (NSArray *)nextBundle;
 
 @end
