30c9ebd49b3cfb4abb3cefdb36e967503e0445c2
Christoph Wendt <chwend@microsoft.com>
2015-02-17 14:11:36 +0100

    Add class for keeping track of network status

---
+++ Classes/MSAIReachability.h
@@ -0,0 +1,13 @@
+//
+//  MSAIReachability.h
+//  AppInsights
+//
+//  Created by GrafZahl on 17/02/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MSAIReachability : NSObject
+
+@end
---
+++ Classes/MSAIReachability.m
@@ -0,0 +1,13 @@
+//
+//  MSAIReachability.m
+//  AppInsights
+//
+//  Created by GrafZahl on 17/02/15.
+//
+//
+
+#import "MSAIReachability.h"
+
+@implementation MSAIReachability
+
+@end
