983f501dd4df60fcf90880d63be8f242899d2a2a
Christoph Wendt <chwend@microsoft.com>
2015-02-25 18:04:37 +0100

    Set maxBatchCount to 50

--- Classes/MSAIChannel.m
+++ Classes/MSAIChannel.m
@@ -16,7 +16,7 @@
 static NSInteger const defaultMaxBatchCount = 5;
 static NSInteger const defaultBatchInterval = 15;
 #else
-static NSInteger const defaultMaxBatchCount = 5;
+static NSInteger const defaultMaxBatchCount = 50;
 static NSInteger const defaultBatchInterval = 15;
 #endif
 
