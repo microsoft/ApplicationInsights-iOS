91755425b28d569fba51c578e6154724f47f3dec
Christoph Wendt <chwend@microsoft.com>
2015-03-17 19:04:50 +0100

    Add implementation + helper methods

--- Classes/MSAISessionHelper.h
+++ Classes/MSAISessionHelper.h
@@ -12,7 +12,7 @@
  *  @param sessionId the sessionId (value)
  *  @param timestamp the timestamp, which represents the creation of the session
  */
-- (void)addSessionId:(NSString *)sessionId WithTimestamp:(NSString *)timestamp;
+- (void)addSessionId:(NSString *)sessionId WithTimestamp:(NSNumber *)timestamp;
 
 ///-----------------------------------------------------------------------------
 /// @name Getting a session
@@ -26,7 +26,7 @@
  *
  *  @return the sessionId of the session, in which the crash occured
  */
-- (NSString *)sessionIdForTimestamp:(NSString *)timestamp;
+- (NSString *)sessionIdForTimestamp:(NSNumber *)timestamp;
 
 ///-----------------------------------------------------------------------------
 /// @name Removing sessions
--- Classes/MSAISessionHelper.m
+++ Classes/MSAISessionHelper.m
@@ -10,6 +10,8 @@ static char *const MSAISessionOperationsQueue = "com.microsoft.appInsights.sessi
 @property (nonatomic, strong) NSString *filePath;
 @property (nonatomic, strong) NSFileManager *fileManager;
 @property (nonatomic, strong) dispatch_queue_t operationsQueue;
+@property (nonatomic, strong) NSMutableDictionary *sessionEntries;
+@property (nonatomic, strong) NSArray *sortDescriptors;
 
 @end
 
@@ -30,15 +32,114 @@ static char *const MSAISessionOperationsQueue = "com.microsoft.appInsights.sessi
 
 - (instancetype)init {
   if(self = [super init]) {
+    NSSortDescriptor *dateSort= [NSSortDescriptor sortDescriptorWithKey:@"self" ascending:NO];
+    _sortDescriptors = [NSArray arrayWithObject:dateSort];
     _operationsQueue = dispatch_queue_create(MSAISessionOperationsQueue, DISPATCH_QUEUE_SERIAL);
     _fileManager = [NSFileManager new];
     _filePath = [self createFilePath];
     [self createPropertyListIfNeeded];
+    [self loadFile];
   }
   return self;
 }
 
+#pragma mark - edit property list
+
+- (void)addSessionId:(NSString *)sessionId WithTimestamp:(NSNumber *)timestamp {
+  
+  __weak typeof(self) weakSelf = self;
+  dispatch_sync(self.operationsQueue, ^{
+    typeof(self) strongSelf = weakSelf;
+    
+    [strongSelf.sessionEntries setObject:sessionId forKey:timestamp];
+  });
+}
+
+- (NSString *)sessionIdForTimestamp:(NSNumber *)timestamp {
+  __block NSString *sessionId = nil;
+  
+  __weak typeof(self) weakSelf = self;
+  dispatch_sync(self.operationsQueue, ^{
+    typeof(self) strongSelf = weakSelf;
+    
+    NSNumber *sessionKey = [strongSelf keyForTimestamp:timestamp];
+    sessionId = [strongSelf.sessionEntries valueForKey:[sessionKey stringValue]];
+  });
+  
+  return sessionId;
+}
+
+- (void)removeSessionId:(NSString *)sessionId {
+  
+  __weak typeof(self) weakSelf = self;
+  dispatch_sync(self.operationsQueue, ^{
+    typeof(self) strongSelf = weakSelf;
+    
+    // Find key for given sessionId
+    // TODO: Maybe sorting dictionary keys in advance is faster
+    NSString *sessionKey = nil;
+    for(NSString *key in _sessionEntries) {
+      if([strongSelf.sessionEntries[key] isEqualToString:sessionId]) {
+        sessionKey = key;
+        break;
+      }
+    }
+    
+    // Remove entry
+    if(sessionKey){
+      [strongSelf.sessionEntries removeObjectForKey:sessionKey];
+    }
+  });
+}
+
+- (void)cleanUpSessionIds {
+  
+  __weak typeof(self) weakSelf = self;
+  dispatch_sync(self.operationsQueue, ^{
+    typeof(self) strongSelf = weakSelf;
+    
+    // Get most recent session
+    NSArray *sortedKeys = [strongSelf sortedKeys];
+    NSInteger lastIndex = [sortedKeys indexOfObject:[sortedKeys lastObject]];
+    NSString *lastKey = sortedKeys[lastIndex];
+    
+    // Clear list and add most recent session
+    NSString *lastValue = strongSelf.sessionEntries[lastKey];
+    [strongSelf.sessionEntries removeAllObjects];
+    [strongSelf.sessionEntries setObject:lastValue forKey:lastKey];
+  });
+}
+
 #pragma mark - Helper
+
+- (NSNumber *)keyForTimestamp:(NSNumber *)timestamp {
+  
+  for(NSNumber *key in [self sortedKeys]){
+    if([self iskey:key forTimestamp:timestamp]){
+      return timestamp;
+    }
+  }
+  return nil;
+}
+
+- (NSArray *)sortedKeys {
+  NSMutableArray *keys = [[_sessionEntries allKeys] mutableCopy];
+  [keys sortUsingDescriptors:_sortDescriptors];
+  
+  return keys;
+}
+
+- (BOOL)iskey:(NSNumber *)key forTimestamp:(NSNumber *)timestamp {
+  return timestamp > key;
+}
+
+- (void)saveFile {
+  [_sessionEntries writeToFile:_filePath atomically:YES];
+}
+
+- (void)loadFile {
+  _sessionEntries = [[NSMutableDictionary alloc] initWithContentsOfFile: _filePath];
+}
 
 - (NSString *)createFilePath{
   NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES); //1
