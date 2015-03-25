b0ed9e8cd40c438d5483d3c1823739fa1c6a8c13
Christoph Wendt <chwend@microsoft.com>
2015-03-25 13:20:17 +0100

    Use default url rather than param value

--- Classes/MSAIAppClient.m
+++ Classes/MSAIAppClient.m
@@ -21,12 +21,10 @@
   
   NSParameterAssert(method);
   NSParameterAssert(params == nil || [method isEqualToString:@"POST"] || [method isEqualToString:@"GET"]);
-  // TODO: Since we are currently talking to two different endpoints, we have to pass the whole address rather than just the path
-  //  NSParameterAssert(self.baseURL);
-  //  path = path ? : @"";
-  //
-  //  NSURL *endpoint = [self.baseURL URLByAppendingPathComponent:path];
-  NSURL *endpoint = [NSURL URLWithString:path];
+    NSParameterAssert(self.baseURL);
+    path = path ? : @"";
+  
+  NSURL *endpoint = [self.baseURL URLByAppendingPathComponent:path];
   NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:endpoint];
   request.HTTPMethod = method;
   
