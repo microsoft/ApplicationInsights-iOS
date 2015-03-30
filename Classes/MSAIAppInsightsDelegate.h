d700e3275d7c48285eea6419404724e247b70b75
Christoph Wendt <chwend@microsoft.com>
2015-03-30 13:49:23 +0200

    Add MSAIAppInsightsDelegate

---
+++ Classes/MSAIAppInsightsDelegate.h
@@ -0,0 +1,13 @@
+//
+//  MSAIAppInsightsDelegate.h
+//  AppInsights
+//
+//  Created by GrafZahl on 30/03/15.
+//
+//
+
+#ifndef AppInsights_MSAIAppInsightsDelegate_h
+#define AppInsights_MSAIAppInsightsDelegate_h
+
+
+#endif
