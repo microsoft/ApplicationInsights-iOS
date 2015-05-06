1fb9164ea313624792b073e369f0c292d018ccff
Lukas Spieß <lspiess@microsoft.com>
2015-05-06 18:05:57 +0200

    Persist current dataItemQueue when app goes into background

--- Classes/MSAITelemetryManager.m
+++ Classes/MSAITelemetryManager.m
@@ -32,6 +32,7 @@
 static char *const MSAITelemetryEventQueue = "com.microsoft.ApplicationInsights.telemetryEventQueue";
 
 @implementation MSAITelemetryManager{
+  id _appDidEnterBackgroundObserver;
   id _sessionStartedObserver;
   id _sessionEndedObserver;
 }
@@ -66,24 +67,33 @@ static char *const MSAITelemetryEventQueue = "com.microsoft.ApplicationInsights.
   NSNotificationCenter *center = [NSNotificationCenter defaultCenter];
   __weak typeof(self) weakSelf = self;
   
+  if (!_appDidEnterBackgroundObserver) {
+    _appDidEnterBackgroundObserver = [center addObserverForName:UIApplicationDidEnterBackgroundNotification
+                                                         object:nil
+                                                          queue:NSOperationQueue.mainQueue
+                                                     usingBlock:^(NSNotification *notification) {
+                                                       [[MSAIChannel sharedChannel] persistDataItemQueue];
+                                                     }];
+  }
+  
   if(!_sessionStartedObserver){
-    [center addObserverForName:MSAISessionStartedNotification
-                        object:nil
-                         queue:NSOperationQueue.mainQueue
-                    usingBlock:^(NSNotification *notification) {
-                      typeof(self) strongSelf = weakSelf;
-                      [strongSelf trackSessionStart];
-                    }];
+    _sessionStartedObserver = [center addObserverForName:MSAISessionStartedNotification
+                                                  object:nil
+                                                   queue:NSOperationQueue.mainQueue
+                                              usingBlock:^(NSNotification *notification) {
+                                                typeof(self) strongSelf = weakSelf;
+                                                [strongSelf trackSessionStart];
+                                              }];
   }
   if(!_sessionEndedObserver){
-    [center addObserverForName:MSAISessionEndedNotification
-                        object:nil
-                         queue:NSOperationQueue.mainQueue
-                    usingBlock:^(NSNotification *notification) {
-                      typeof(self) strongSelf = weakSelf;
-
-                      [strongSelf trackSessionEnd];
-                    }];
+    _sessionEndedObserver = [center addObserverForName:MSAISessionEndedNotification
+                                                object:nil
+                                                 queue:NSOperationQueue.mainQueue
+                                            usingBlock:^(NSNotification *notification) {
+                                              typeof(self) strongSelf = weakSelf;
+                                              
+                                              [strongSelf trackSessionEnd];
+                                            }];
   }
 }
 
@@ -192,7 +202,7 @@ static char *const MSAITelemetryEventQueue = "com.microsoft.ApplicationInsights.
 
 - (void)trackException:(NSException *)exception{
   pthread_t thread = pthread_self();
-
+  
   dispatch_async(_telemetryEventQueue, ^{
     PLCrashReporterSignalHandlerType signalHandlerType = PLCrashReporterSignalHandlerTypeBSD;
     PLCrashReporterSymbolicationStrategy symbolicationStrategy = PLCrashReporterSymbolicationStrategyAll;
@@ -256,7 +266,7 @@ static char *const MSAITelemetryEventQueue = "com.microsoft.ApplicationInsights.
 - (void)trackSessionStart {
   MSAISessionStateData *sessionState = [MSAISessionStateData new];
   sessionState.state = MSAISessionState_start;
-
+  
   if(![[MSAIChannel sharedChannel] isQueueBusy]){
     MSAIEnvelope *envelope = [[MSAIEnvelopeManager sharedManager] envelopeForTelemetryData:sessionState];
     envelope.tags[@"ai.session.isNew"] = @"true";
