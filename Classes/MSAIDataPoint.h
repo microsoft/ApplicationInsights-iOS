2ad2203e0efcbb563ba857dbdae870e169296ab8
Benjamin Reimold <bereimol@microsoft.com>
2015-07-24 14:34:35 +0200

    Make sure MSAIDataPoint is not a pointer. (Removes warnings during unit test and possible bugs)

--- Classes/MSAIDataPoint.h
+++ Classes/MSAIDataPoint.h
@@ -6,7 +6,7 @@
 @interface MSAIDataPoint : MSAIObject <NSCoding>
 
 @property (nonatomic, copy) NSString *name;
-@property (nonatomic, assign) MSAIDataPointType *kind;
+@property (nonatomic, assign) MSAIDataPointType kind;
 @property (nonatomic, strong) NSNumber *value;
 @property (nonatomic, strong) NSNumber *count;
 @property (nonatomic, strong) NSNumber *min;
